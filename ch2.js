60f0e99f8bcc945655378cda3629687620ccea74
Erika Miguel <erika@erikamiguel.com>
2019-09-16 22:24:22 -0400

    insert at

--- ch2.js
+++ ch2.js
@@ -13,6 +13,7 @@ class Node {
 class LinkedList {
     constructor() {
         this.head = null;
+        this.size = 0;
     }
 
     printLinkedList() {
@@ -23,6 +24,17 @@ class LinkedList {
             curr = curr.next;
         }
         console.log(string);
+    }
+
+    get(idx) {
+        if (idx > this.size) return -1;
+        if (idx === 0) return this.head;
+        let currIdx = 0, curr = this.head;
+        while(curr && currIdx !== idx) {
+            curr = curr.next;
+            currIdx++;
+        }
+        return curr;
     }
 }
 
@@ -44,6 +56,27 @@ class SinglyLinkedList extends LinkedList {
 
         let last = curr;
         last.next = new Node(elem);
+        this.size++;
+    }
+
+    // Do we want this function to support adding to the tail?
+    insertAt(elem, idx) {
+        let newNode = new Node(elem);
+        if (idx === 0) {
+            let oldHead = this.head;
+            newNode.next = oldHead;
+            this.head = newNode;
+            this.size++;
+            return;
+        }
+
+        let old = this.get(idx);
+        let oldPrev = null;
+        if(idx - 1 > -1) oldPrev = this.get(idx - 1)
+
+        if (oldPrev) oldPrev.next = newNode;
+        newNode.next = old;
+        this.size++;
     }
 }
 
@@ -52,4 +85,10 @@ sLL.add(10);
 sLL.add(20);
 sLL.add(30);
 sLL.add(40);
+sLL.printLinkedList();
+sLL.insertAt(15, 1);
+sLL.printLinkedList();
+sLL.insertAt(5, 0);
+sLL.printLinkedList();
+sLL.insertAt(35, 5);
 sLL.printLinkedList();
