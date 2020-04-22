9ea34ef2c67f97247a50f23a41e7ad997ef2747d
Erika Miguel <erika@erikamiguel.com>
2020-04-22 14:14:33 -0400

    dfs bfs

--- graph.js
+++ graph.js
@@ -7,6 +7,13 @@ class BinaryTreeNode {
 }
 
 // let's make a heap
+/**
+ *             4
+ *          /     \
+ *         50      7
+ *        /  \    /
+ *       55   90 87
+ */
 let root = new BinaryTreeNode(4);
 root.left = new BinaryTreeNode(50);
 root.left.left = new BinaryTreeNode(55);
@@ -27,6 +34,39 @@ function inorderTraversal(node) {
     return arr;
 }
 
+function dfs(node) {
+    let visited = [];
+    function dfsUtil(node){
+        if (node !== null) {
+            dfsUtil(node.left);
+            dfsUtil(node.right);
+            visited.push(node.val);
+        }
+    }
+    dfsUtil(node);
+    return visited;
+}
+
+function bfs(node) {
+    let queue = [];
+    let visited = [];
+    queue.push(node);
+    while (queue.length > 0) {
+        let current = queue.shift();
+        visited.push(current.val);
+        current.visited = true;
+        if (current.left && (!(current.left.hasOwnProperty('visited')) || !current.left.visited)) {
+            queue.push(current.left);
+        }
+        if (current.right && (!(current.right.hasOwnProperty('visited')) || !current.right.visited)) {
+            queue.push(current.right);
+        }
+    }
+    return visited;
+}
+
 
 
 console.log(inorderTraversal(root))
+console.log(dfs(root));
+console.log(bfs(root))
--- search.js
+++ search.js
@@ -29,14 +29,47 @@ function binarySearchRecursive(arr, x, low, high) {
     }
 }
 
+function shiftedArraySearch(arr, num) {
+    function findPivot(){
+        let low = 0;
+        let high = arr.length - 1;
+        
+        while (low <= high) {
+            let mid = Math.floor((low+high)/2);
+            if (mid === 0 || arr[mid] < arr[mid - 1]){
+                return mid;
+            } else if (arr[0] < arr[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1; // arr[0] > arr[mid]
+            }
+        }
+
+        return 0;
+    }
+    let pivot = findPivot();
+    if (pivot === 0 || num < arr[0]) {
+        return binarySearchRecursive(arr, num, pivot + 1, arr.length - 1);
+    }
+
+    return binarySearchRecursive(arr, num, 0, pivot);
+
+}
+
 console.log(binarySearchIterative([1,2,3,4,7,12],7));
 console.log(binarySearchIterative([1,2,3,4,7,12],1));
 console.log(binarySearchIterative([1,2,3,4,7,12],2));
 console.log(binarySearchIterative([1,2,3,4,7,12],12));
 console.log(binarySearchIterative([1,2,3,4,7,12],5));
+console.log('------------------------------------\n');
 
 console.log(binarySearchRecursive([1,2,3,4,7,12],7,0,5));
 console.log(binarySearchRecursive([1,2,3,4,7,12],1,0,5));
 console.log(binarySearchRecursive([1,2,3,4,7,12],2,0,5));
 console.log(binarySearchRecursive([1,2,3,4,7,12],12,0,5));
 console.log(binarySearchRecursive([1,2,3,4,7,12],5,0,5));
+console.log('------------------------------------\n');
+
+console.log(shiftedArraySearch([9, 12, 17, -2, -1, 1, 2, 4, 5, 6, 7], 4));
+console.log(shiftedArraySearch([9, 12, 17, 2, 4, 5], 9))
+console.log(shiftedArraySearch([1,2,3,4,7,12],3));
